47649a0e9c91b77430cf74046e6b6700a8ce6ab7
꧁☬⋆cσɱɾα∂ε༒αcɦαყαɳ⋆☬꧂ <70142311+Comrade-Achayan@users.noreply.github.com>
2021-08-11 10:38:18 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -28,6 +28,7 @@ async def start(bot, cmd):
                 await bot.send_message(
                     chat_id=cmd.from_user.id,
                     text="<b>♦️ READ THIS INSTRUCTION ♦️</b>\n\n<i>🗣 നിങ്ങൾ ചോദിക്കുന്ന സിനിമകൾ നിങ്ങൾക്ക് ലഭിക്കണം എന്നുണ്ടെങ്കിൽ നിങ്ങൾ താഴെ കൊടുത്തിട്ടുള്ള ചാനലിൽ ജോയിൻ ചെയ്യണം. ജോയിൻ ചെയ്ത ശേഷം വീണ്ടും ഗ്രൂപ്പിൽ പോയി ആ ബട്ടനിൽ അമർത്തിയാൽ നിങ്ങൾക്ക് ഞാൻ ആ സിനിമ പ്രൈവറ്റ് ആയി അയച്ചു തരുന്നതാണ്..😍\n\n🗣 In Order To Get The Movie Requested By You in Our Groups, You Will Have To Join Our Official Channel First. After That, Try Accessing That Movie Again From Our Group. I'll Send You That Movie Privately 🙈</i>\n\n<b>👇 JOIN THIS CHANNEL & TRY 👇\n\n[https://t.me/joinchat/w_dsk1rXMlBmY2M1]</b>",
+                    disable_web_page_preview=True,
                     reply_markup=InlineKeyboardMarkup(
                         [
                             [
