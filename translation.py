4ff02d308dfdd5fff8c25807aac61918e131d3d1
꧁☬⋆cσɱɾα∂ε༒αcɦαყαɳ⋆☬꧂ <70142311+Comrade-Achayan@users.noreply.github.com>
2021-08-15 10:31:31 +0530

    Update translation.py

--- translation.py
+++ translation.py
@@ -1,5 +1,5 @@
 class Translation(object):
     
-    START_TEXT = """<b>Hey {}!!</b>\n\n<i>I'm a simple bot for autofilters group and maintained by <a href="tg://user?id=965407125">This Person.</a></i>"""
+    START_TEXT = """<b>Hey {} My Name is {}.</b>\n\n<i>I'm a simple bot for autofilters group and maintained by <a href="tg://user?id=965407125">This Person.</a></i>"""
 
 
