e6adf7abd75ca00980a75b23fa71063783870f52
꧁☬⋆cσɱɾα∂ε༒αcɦαყαɳ⋆☬꧂ <70142311+Comrade-Achayan@users.noreply.github.com>
2021-08-16 17:39:30 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -91,7 +91,7 @@ async def filter(client, message):
             if API_KEY:
                 poster=await get_poster(search)
             if poster:
-                await message.reply_photo(photo=poster, caption=f"<b>Total Files:</b><code>{len(files)}</code>\n<b>Movie Name:</b> <code>{search}</code>blanhyyss\n\n<b>© 𝐐𝐮𝐚𝐥𝐢𝐭𝐲 𝐌𝐨𝐭𝐢𝐨𝐧 𝐏𝐢𝐜𝐭𝐮𝐫𝐞𝐬</b>", reply_markup=InlineKeyboardMarkup(buttons))
+                await message.reply_photo(photo=poster, caption=f"<b>Total Files:</b><code>{len(files)}</code>\n<b>Movie Name:</b> <code>{search}</code>\n\n<b>© 𝐐𝐮𝐚𝐥𝐢𝐭𝐲 𝐌𝐨𝐭𝐢𝐨𝐧 𝐏𝐢𝐜𝐭𝐮𝐫𝐞𝐬</b>", reply_markup=InlineKeyboardMarkup(buttons))
             else:
                 await message.reply_photo(photo=FILTER_PIC, caption=f"<b>Total Files:</b><code>{len(files)}</code>\n<b>Movie Name:</b> <code>{search}</code>\n\n<b>© 𝐐𝐮𝐚𝐥𝐢𝐭𝐲 𝐌𝐨𝐭𝐢𝐨𝐧 𝐏𝐢𝐜𝐭𝐮𝐫𝐞𝐬</b>", reply_markup=InlineKeyboardMarkup(buttons))
             return
@@ -112,7 +112,7 @@ async def filter(client, message):
         if API_KEY:
             poster=await get_poster(search)
         if poster:
-            await message.reply_photo(photo=poster, caption=f"<b>Total Files:</b><code>{len(files)}</code>\n<b>Movie Name:</b> <code>{search}</code>\n\n<b© 𝐐𝐮𝐚𝐥𝐢𝐭𝐲 𝐌𝐨𝐭𝐢𝐨𝐧 𝐏𝐢𝐜𝐭𝐮𝐫𝐞𝐬</b>", reply_markup=InlineKeyboardMarkup(buttons))
+            await message.reply_photo(photo=poster, caption=f"<b>Total Files:</b><code>{len(files)}</code>\n<b>Movie Name:</b> <code>{search}</code>hshdhdhddhhddh\n\n<b© 𝐐𝐮𝐚𝐥𝐢𝐭𝐲 𝐌𝐨𝐭𝐢𝐨𝐧 𝐏𝐢𝐜𝐭𝐮𝐫𝐞𝐬</b>", reply_markup=InlineKeyboardMarkup(buttons))
         else:
             await message.reply_photo(photo=FILTER_PIC, caption=f"<b>Total Files:</b><code>{len(files)}</code>\n<b>Movie Name:</b> <code>{search}</code>\n\n<b>© 𝐐𝐮𝐚𝐥𝐢𝐭𝐲 𝐌𝐨𝐭𝐢𝐨𝐧 𝐏𝐢𝐜𝐭𝐮𝐫𝐞𝐬</b>", reply_markup=InlineKeyboardMarkup(buttons))
 
