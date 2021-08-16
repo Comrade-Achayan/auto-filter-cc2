a2f6070972154d9416211fd67deae3abf915f4ed
꧁☬⋆cσɱɾα∂ε༒αcɦαყαɳ⋆☬꧂ <70142311+Comrade-Achayan@users.noreply.github.com>
2021-08-16 11:14:30 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -195,7 +195,7 @@ async def group(client, message):
 def get_size(size):
     """Get size in readable format"""
 
-    units = ["Bytes", "KB", "MB", "GB", "TB", "PB", "EB"]8
+    units = ["Bytes", "KB", "MB", "GB", "TB", "PB", "EB"]
     size = float(size)
     i = 0
     while size >= 1024.0 and i < len(units):
